68bfbb4e92f9e465f827d4d27828f82f90e1f7cd
Mahedhar-35 <24cs01035@iitbbs.ac.in>
2026-05-31 21:23:42 +0530

    Time: 134 ms (20.69%), Space: 21 MB (89.66%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def asteroidsDestroyed(self, mass, asteroids):
+        """
+        :type mass: int
+        :type asteroids: List[int]
+        :rtype: bool
+        """
+        asteroids=sorted(asteroids)
+        count=mass
+        for i in range(len(asteroids)):
+            if count>=asteroids[i]:
+                count+=asteroids[i]
+            else:
+                return False
+        return True            
