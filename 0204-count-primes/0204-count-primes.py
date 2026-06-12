3bee69685d82b7335eaf98be201d0c017aa19caf
Mahedhar-35 <24cs01035@iitbbs.ac.in>
2026-06-12 16:32:53 +0530

    Time: 2093 ms (90.49%), Space: 130.4 MB (85.42%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -7,13 +7,13 @@ class Solution(object):
         if n<3:
             return 0
 
-        prime = [True] * (n)
+        prime = [1] * (n)
         p = 2
-        prime[0]=False
-        prime[1]=False
+        prime[0],prime[1]=0,0
+        
         while p * p <= n:
             if prime[p]:
                 for i in range(p * p, n, p):
-                    prime[i] = False
+                    prime[i] = 0
             p += 1
         return sum(prime)
