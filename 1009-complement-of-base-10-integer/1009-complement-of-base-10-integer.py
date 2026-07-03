2ebe691b35005a00b99938bd6d964f3c72bbf6b1
Maheedhar <24cs01035@iitbbs.ac.in>
2026-07-03 11:16:26 +0530

    Time: 0 ms (100%), Space: 12.4 MB (52.06%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def bitwiseComplement(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        mask = 1
+        
+        while mask < n:
+            mask = (mask << 1) | 1
+        return n ^ mask
