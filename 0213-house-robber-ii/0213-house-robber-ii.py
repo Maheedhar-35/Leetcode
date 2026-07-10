36ab0b080c90b1e7d80ee2b998bdb2b18920d996
Maheedhar <24cs01035@iitbbs.ac.in>
2026-07-10 23:01:32 +0530

    Time: 0 ms (100%), Space: 12.2 MB (91.1%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -10,6 +10,5 @@ class Solution(object):
         dp1[0], dp1[1], dp2[0], dp2[1]= 0, nums[0], 0, nums[1]
         for i in range(2,len(nums)):
             dp1[i]=max(dp1[i-1],dp1[i-2]+nums[i-1])
-        for i in range(2,len(nums)):
             dp2[i]=max(dp2[i-1],dp2[i-2]+nums[i])
         return max(dp1[len(nums)-1],dp2[len(nums)-1])    
