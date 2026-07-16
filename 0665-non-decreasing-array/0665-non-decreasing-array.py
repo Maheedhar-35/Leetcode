6ad9a6b4b353e460f70daf35a37291cb709dbabd
Maheedhar <24cs01035@iitbbs.ac.in>
2026-07-16 18:13:48 +0530

    Time: 0 ms (100%), Space: 13.3 MB (55.43%) - LeetHub

---
+++ 0665-non-decreasing-array/0665-non-decreasing-array.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def checkPossibility(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        result=False
+        for i in range(len(nums)-1):
+            if nums[i]<=nums[i+1]:
+                continue
+            
+            if result:
+                return False
+            result=True  
+
+            if i == 0 or nums[i - 1] <= nums[i + 1]:
+                nums[i] = nums[i + 1]  
+            else:
+                nums[i + 1] = nums[i]  
+        return True        
+
+            
