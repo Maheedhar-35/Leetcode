717970be2bd33160a06f14db59170719408b5fcc
Maheedhar <24cs01035@iitbbs.ac.in>
2026-07-18 18:36:31 +0530

    Time: 0 ms (100%), Space: 13.2 MB (95.94%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        low ,high =0,len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]>target:
+                high=mid-1
+            elif nums[mid]<target:
+                low=mid+1 
+            else:
+                return mid
+        return -1               
